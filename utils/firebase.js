32c3bab87cd9c548c0cd88f3dde4c8095559a7c7
DILEEPSAJJA <sajjadileep2003@gmail.com>
2024-11-06 20:42:56 +0530

    Feat: Final App Fix

--- utils/firebase.js
+++ utils/firebase.js
@@ -14,70 +14,13 @@ const firebaseConfig = {
   measurementId: "G-ECJZKP1MDW"
 };
 
-// Initialize Firebase app
-const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Auth with persistence using AsyncStorage
+const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage)
 });
 
-// Initialize Firestore
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
 export { auth, firestore, storage };
-
-
-
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB479rzeQ4NdyAfQRVz5hMwbm2dZNxZpsg",
-//   authDomain: "miniproject-1bb95.firebaseapp.com",
-//   projectId: "miniproject-1bb95",
-//   storageBucket: "miniproject-1bb95.appspot.com",
-//   messagingSenderId: "43493497023",
-//   appId: "1:43493497023:web:b6d34c1a113800679c8e0a",
-//   measurementId: "G-ECJZKP1MDW"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const firestore = getFirestore(app);
-
-// export { auth, firestore };
-
-
-// import { initializeApp } from 'firebase/app';
-// import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// // Your Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB479rzeQ4NdyAfQRVz5hMwbm2dZNxZpsg",
-//   authDomain: "miniproject-1bb95.firebaseapp.com",
-//   projectId: "miniproject-1bb95",
-//   storageBucket: "miniproject-1bb95.appspot.com",
-//   messagingSenderId: "43493497023",
-//   appId: "1:43493497023:web:b6d34c1a113800679c8e0a",
-//   measurementId: "G-ECJZKP1MDW"
-// };
-
-// // Initialize Firebase app
-// const app = initializeApp(firebaseConfig);
-
-// // Initialize Firebase Auth with persistence using AsyncStorage
-// const auth = initializeAuth(app, {
-//   persistence: getReactNativePersistence(AsyncStorage)
-// });
-
-// // Initialize Firestore
-// const firestore = getFirestore(app);
-// const storage = getStorage(app);
-
-// export { auth, firestore, storage };
